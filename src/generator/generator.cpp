b9a31f0fca0262504e1b75cbf5e6ad44350ea100
Jorge Teixeira <matosjorge88@gmail.com>
2024-04-05 12:46:56 +0100

    Update generator.cpp

--- src/generator/generator.cpp
+++ src/generator/generator.cpp
@@ -53,7 +53,6 @@ int main(int argc, char *argv[]){
         cout << "sphere              | {radius} {slices} {stacks}                 | {filename}.3d" << endl;
         cout << "cylinder            | {radius} {height} {slices}                 | {filename}.3d" << endl;
         cout << "torus               | {radius_in} {radius_out} {slices} {stacks} | {filename}.3d" << endl;
-        cout << "ellipsoid           | {a} {b} {c} {slices} {stacks}              | {filename}.3d" << endl;
         cout << "--------------------------------------------------------------------------------" << endl;
         
         return 1;
@@ -73,7 +72,6 @@ int main(int argc, char *argv[]){
     char erSphere[] = "sphere ([0-9]+[.])?[0-9]+ [0-9]+ [0-9]+ [a-zA-Z0-9_]+\\.3d$";
     char erCylinder[] = "cylinder ([0-9]+[.])?[0-9]+ ([0-9]+[.])?[0-9]+ [0-9]+ [a-zA-Z0-9_]+\\.3d$";
     char erTorus[] = "torus ([0-9]+[.])?[0-9]+ ([0-9]+[.])?[0-9]+ [0-9]+ [0-9]+ [a-zA-Z0-9_]+\\.3d$";
-    char erEllipsoid[] = "ellipsoid ([0-9]+[.])?[0-9]+ ([0-9]+[.])?[0-9]+ ([0-9]+[.])?[0-9]+ [0-9]+ [0-9]+ [a-zA-Z0-9_]+\\.3d$";
 
     if (regex_match(inp, regex(erPlane))){
         float length = atof(argv[2]);
@@ -128,17 +126,6 @@ int main(int argc, char *argv[]){
 
         pair<vector<Point>, vector<Triangle> > torus = generateTorus(radiusIn, radiusOut, slices, stacks);
         toFile(filename, torus);
-    }
-    else if(regex_match(inp, regex(erEllipsoid))){
-        float a = atof(argv[2]);
-        float b = atof(argv[3]);
-        float c = atof(argv[4]);
-        int slices = atoi(argv[5]);
-        int stacks = atoi(argv[6]);
-        char *filename = argv[7];
-
-        pair<vector<Point>, vector<Triangle> > ellipsoid = generateEllipsoid(a, b, c, slices, stacks);
-        toFile(filename, ellipsoid);
     }
     else cout << "Invalid input!" << endl;
 
